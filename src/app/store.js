07a94ec9bf5c28599cd84c0da8b931db850e9c35
priyadarshiNishant <priyadarshinishant063@gmail.com>
2024-01-14 08:28:15 +0530

    tesla

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import carReducer from "../features/car/carSlice";
+
+export const store = configureStore({
+    reducer: {
+        car: carReducer,
+    },
+});
